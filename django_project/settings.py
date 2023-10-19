995abde097656c10f82edec0d9fc53ce6fc9635b
Matt <>
2023-10-18 18:18:28 -0600

    fixes

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,6 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-SECRET_KEY
-# SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2"
 SECRET_KEY = env("SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
